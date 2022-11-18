876091eb83b6104ae4c5ce52dc7b5a55a8a545b4
R0885807 <robbe.verbist@student.kuleuven.be>
2022-11-18 18:22:23 +0100

    addition cors

--- app/guitar.py
+++ app/guitar.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI, Query, Path
 from pydantic import BaseModel, Field
 from random import randint
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "https://localhost.tiangolo.com"
+    "http://172.0.0.1:5500",
+    "https://r0885807.github.io/guitar"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Guitar(BaseModel):
     name: str = Field(max_length=100, title= "The name of the guitar")
